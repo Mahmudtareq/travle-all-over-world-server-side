692b168cf41373c6608d5536bf57c2f7e17704cd
Mahmudtareq <tareqlpu5@gmail.com>
2022-01-11 00:50:52 +0600

    send product to database

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { MongoClient } = require('mongodb');
 const ObjectId = require('mongodb').ObjectId;
 const cors = require('cors');
 require('dotenv').config();
-
+// 
 const app = express();
 const port =process.env.PORT || 5000;
 
